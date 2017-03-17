fa3ca8a36c40098b5f25c77b99780eb8965f184c
Alex Thomas <home@Alexs-MacBook-Pro.local>
2017-03-18 01:30:32 +0900

    fixed game loop to end when proper height is reached on tower 3.

--- toh.rb
+++ toh.rb
@@ -14,9 +14,17 @@ class Toh
 	def setup
 		puts "How tall should the tower be?"
 		@height = gets.chomp.to_i
+		# test height of tower
+		if @height > 2  
+			@height = @height
+		else
+			puts "Height must be more than 2!"
+			setup
+		end 		
 	end
 
-		#make array for 1st tower
+	
+	#make array for 1st tower
 	def setup_tower
 		puts "               TOWER OF HANOI!!!"
 		@tower_1 = (1..@height).to_a.reverse.map { |e| e.to_i}
@@ -39,7 +47,6 @@ class Toh
 		first_move = gets.chomp.to_i
 		if first_move.between?(1, 3) 
 			@first_choice = first_move 
-			puts "gets first input"
 		else
 			puts "You must enter  number between 1 and 3"
 			get_first_input
@@ -51,7 +58,6 @@ class Toh
 		second_move = gets.chomp.to_i
 		if second_move.between?(1, 3) 
 			@second_choice = second_move.to_i 
-			puts "gets second input"
 		else
 			puts "You must enter  number between 1 and 3"
 			get_second_input
@@ -60,7 +66,6 @@ class Toh
 
 		#checks if tower is empty
 	def check_first_choice
-		puts "checks first choice"
 		if @first_choice == 1 && @tower_1.length > 0
 			@moving_item = @tower_1.last
 		elsif @first_choice == 2 && @tower_2.length > 0
@@ -71,39 +76,22 @@ class Toh
 			puts "Can't choose an empty tower!"
 			get_first_input
 			check_first_choice
-			puts "end else first_choice"
 			
 		end
 	end
 
 	#check if second choice top item is greater than @first_choice
 	def check_second_choice
-		puts "check second choice"
-		puts " this is @second_choice: #{@second_choice}"
-		puts " this is @second_choice.class: #{@second_choice.class}"
-		puts " this is @tower_2.last: #{@tower_2.last}"
-		puts " this is @tower_2.last.class: #{@tower_2.last.class}"
-		puts "moving item: #{@moving_item}"
-		if @second_choice == 1  
-			puts "testing second choice here" 
-		elsif @second_choice == 2
-			puts "testing if 2"
-		end
-		
 		if @second_choice == 1 && (@tower_1.last.to_i >= @moving_item.to_i || @tower_1.last == nil)
 			@moving_item = @moving_item
-			puts "if secondchoice 1"
 		elsif @second_choice == 2 && (@tower_2.last.to_i >= @moving_item.to_i || @tower_2.last == nil)
 			@moving_item = @moving_item			
-			puts "if secondchoice 2"
 		elsif @second_choice == 3	&& (@tower_3.last.to_i >= @moving_item.to_i || @tower_3.last == nil)
 			@moving_item = @moving_item
-			puts "if secondchoice 3"
 		else
 			puts "Can't put a number above a lower value!"
 			get_second_input
 			check_second_choice
-			puts "end else second_choice"
 		end
 	end
 
@@ -114,26 +102,20 @@ class Toh
 		#remove first choice from tower
 		if @first_choice == 1 
 			@item = @tower_1.pop
-			puts "pop 1"
 		elsif @first_choice == 2 
 			@item = @tower_2.pop
-			puts "pop 2"
 		elsif @first_choice == 3 
 			@item = @tower_3.pop 
-			puts "pop 3"
 		end
 		#add item to second_choice tower
 		if @second_choice == 1 
 			@tower_1 << @item
-			puts "add to array 1"
 		elsif @second_choice == 2 
 			@tower_2 << @item
-			puts "add to array 2"
 			#@tower_2.reverse!
 			#@tower_2.pop
 		elsif @second_choice == 3	
 			@tower_3 << @item
-			puts "add to array 3"
 			#@tower_3.pop
 		end
 	end
@@ -146,31 +128,28 @@ class Toh
 			tower_3_o = 'o' * @tower_3[n].to_i
 	  	#puts "#{`tower_1_o}            #{tower_2_o}           #{tower_3_o} "
 	  	puts "#{@tower_1[n]}                #{@tower_2[n]}             #{@tower_3[n]}"
-	  	n -= 1
+	  	n -= 1		
 		end
-		puts "Tower 1       Tower 2       Tower 3"
+		puts "Tower 1       Tower 2       Tower 3"		
 	end
+
 	def run_game
 		
-		until @tower_3[@height - 1] == "1" do
-			puts "run game 1"
+		until @tower_3[@height - 1] == 1 do
 			get_first_input
-			puts "run game 2"
 			check_first_choice
 			get_second_input
-			puts "run game 3"
 			check_second_choice			
-			puts "run game 4"
 			makes_move
-			puts "run game 5"
 			render
 		end
+	puts "Great Job!!!"	
 	end
-end
+
+end  # End class
 
 start = Toh.new
 start.start_game
 start.setup
 start.setup_tower
 start.run_game
-puts "Great Job!!!"
